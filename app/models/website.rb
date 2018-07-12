d47c93b39eebf3fdf71121a662f34dda41f5693f
raghav <raghav.chakravarthi@freshworks.com>
2018-07-12 19:03:30 +0530

    kj

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,9 +1,3 @@
 class Website < ApplicationRecord
 	belongs_to :subscriber
-
-	def self.db
-		Rails.logger.info "Starting DB"
-		web = Website.create(url: "aaaa", content: "aaaaa", subscriber: Subscriber.first)
-		web.save!
-	end
 end
